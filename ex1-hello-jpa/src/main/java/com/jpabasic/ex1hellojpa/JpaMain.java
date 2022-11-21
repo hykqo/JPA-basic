1019a3f9f127e71d74dca0d31e1c2658eb8d011d
ENOCH YEOM <hykqo@naver.com>
2022-11-21 13:49:38 +0900

    프록시를 활용한 지연로딩 예제

--- ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/JpaMain.java
@@ -12,56 +12,26 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
+
+            Team team  = new Team();
+            team.setName("team1");
+            em.persist(team);
+            
             HelloMember member = new HelloMember();
             member.setUsername("hello1");
+            member.setTeam(team);
             em.persist(member);
-
+            
             em.flush();
             em.clear();
 
-            //proxy 예제
-//            System.out.println("-------getReference 호출 시작-------------");
-//            HelloMember referenceMember = em.getReference(HelloMember.class, member.getId());
-//            System.out.println("-------getReference 호출 종료-------------");
-//            System.out.println("referenceMember.getClass() = " + referenceMember.getClass());
-//            System.out.println("referenceMember = " + referenceMember.getUsername());
+            HelloMember member1 = em.find(HelloMember.class, member.getId());
+            System.out.println("member1.getClass() = " + member1.getClass());
+            System.out.println("member1.getTeam().getClass() = " + member1.getTeam().getClass());
 
-            //find로 조회한 entity객체와 getReference로 조회한 proxy객체 비교.
-            HelloMember m1 = em.find(HelloMember.class, member.getId());
-            em.clear();
-            HelloMember m2 = em.getReference(HelloMember.class, member.getId());
-            System.out.println("findMember1 == referenceMember : " + (m1.getClass() == m2.getClass()));
-            em.clear();
-
-            //영속성 컨텍스트에 찾는 엔티티가 이미 있으면 em.getReference()를 호출해도 실제 엔티티 반환.
-            HelloMember m3 = em.find(HelloMember.class, member.getId());
-            HelloMember m4 = em.getReference(HelloMember.class, member.getId());
-            System.out.println("m3.getClass() = " + m3.getClass());
-            System.out.println("m4.getClass() = " + m4.getClass());
-            em.clear();
-
-            System.out.println("---");
-
-            //영속성 컨텍스트의 도움을 받을 수 없는 준영속 상태일 때, 프록시를 초기화하면 문제 발생
-            HelloMember m5 = em.getReference(HelloMember.class, member.getId());
-            System.out.println("m5.getClass() = " + m5.getClass());
-            HelloMember m6 = em.find(HelloMember.class, member.getId());
-            System.out.println("m6.getClass() = " + m6.getClass());
-            System.out.println("m5 == m6 : " + (m5.getClass() == m6.getClass()));
-            System.out.println(m5.getUsername());
-            em.clear();
-
-            //영속성 컨텍스트의 도움을 받을 수 없는 준영속 상태일 때, 프록시를 초기화하면 문제 발생
-//            HelloMember m7 = em.getReference(HelloMember.class, member.getId());
-//            em.detach(m7);
-//            m7.getUsername();
-
-            //프록시 인스턴스의 초기화 여부 확인기능.
-            HelloMember m8 = em.getReference(HelloMember.class, member.getId());
-            System.out.println("getPersistenceUnitUtil.isLoaded = "+ emf.getPersistenceUnitUtil().isLoaded(m8));
-            Hibernate.initialize(m8);
-            System.out.println("getPersistenceUnitUtil.isLoaded = "+ emf.getPersistenceUnitUtil().isLoaded(m8));
-
+            System.out.println("======");
+            member.getTeam().getName(); //이떄 초기화가 실행됨.
+            System.out.println("======");
             tx.commit();
         }catch (Exception e){
             e.printStackTrace();
--- ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/hellojpa/HelloMember.java
+++ ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/hellojpa/HelloMember.java
@@ -13,8 +13,8 @@ import java.util.List;
     private Long id;
     @Column(name = "USERNAME")
     private String username;
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
     private Team team;
 //    @OneToOne
 //    @JoinColumn(name = "LOCKER_ID")
