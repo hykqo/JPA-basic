412635d6f3ab22f2874e1c0180b170464b67b6fd
ENOCH YEOM <hykqo@naver.com>
2022-11-23 09:18:11 +0900

    값 타입의 비교 - equals()메서드 정의해서 사용

--- ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/hellojpa/ValueMain.java
+++ ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/hellojpa/ValueMain.java
@@ -3,22 +3,13 @@ package com.jpabasic.ex1hellojpa.hellojpa;
 public class ValueMain {
 
     public static void main(String[] args) {
-        //int와 double같은 기본타입(primitive type)은 자바가 sideEffect가 일어나지 않도록 복사처리를 해준다.
         int a = 10;
-        int b = a;
-        b = 20;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        int b = 10;
+        System.out.println("(a==b) = " + (a==b));
 
-
-        Integer c = new Integer(10);
-        Integer d = c;
-
-//        a.setValue(20);
-
-        System.out.println("c = " + c);
-        System.out.println("d = " + d);
-
+        Address address1 = new Address("city", "street", "zipcode");
+        Address address2 = new Address("city", "street", "zipcode");
+        System.out.println("(address1 == address2) = " + (address1.equals(address2)));
 
     }
 }
