a6e9fcb8b6dbab96dd77ea25e349f35fe7c9c280
ENOCH YEOM <hykqo@naver.com>
2022-12-18 15:13:35 +0900

    실전예제 - Delivery 값타입 공통화 작업

---
+++ ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/domain/Address.java
@@ -0,0 +1,61 @@
+package com.jpabasic.ex1hellojpa.domain;
+
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class Address {
+    @Column(length = 10)
+    private String city;
+    @Column(length = 20)
+    private String street;
+    @Column(length = 5)
+    private String zipcode;
+
+    //값타입은 좋은게 여기서 의미있는 메소드를 만들 수 있다.
+    public String fullAddress(){
+        return getCity() + " " + getStreet() +  " " + getZipcode();
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    private void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    //equals()메소드를 짤때에는 프록시일때도 호출할 수 있도록 get을 이용해서 호출한다.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) &&
+                Objects.equals(getStreet(), address.getStreet()) &&
+                Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
+    }
+}
--- ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/domain/Delivery.java
+++ ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/domain/Delivery.java
@@ -11,10 +11,41 @@ public class Delivery extends BaseEntity {
 
     @Id @GeneratedValue
     private Long id;
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
     private DeliveryStatus delieveryStatus;
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public DeliveryStatus getDelieveryStatus() {
+        return delieveryStatus;
+    }
+
+    public void setDelieveryStatus(DeliveryStatus delieveryStatus) {
+        this.delieveryStatus = delieveryStatus;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
