f0486f3dadc69eff724921b44fb867bad229cda6
ENOCH YEOM <hykqo@naver.com>
2023-01-31 01:20:01 +0900

    JPQL 기본함수 예제

--- ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/JpaMain.java
@@ -29,20 +29,25 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            //기본 case식
-            String query = "select " +
-                                    "case when m.age <= 10 then '학생요금' " +
-                                    "     when m.age >= 60 then '학생요금' " +
-                                    "     else '일반요금' " +
-                                    "end " +
-                            "from JMember m";
-            List<String> result =  em.createQuery(query, String.class).getResultList();
 
-            //coalesce 사용 -> 사용자 이름이 없으면 '이름 없는 회원' 반환
-            List<String> result2 = em.createQuery("select coalesce(m.username, '이름 없는 회원') from JMember  m", String.class).getResultList();
+            //JPQL 기본함수
+            //CONCAT - 문자열 A와 B를 더한다
+            List<String> result1 = em.createQuery("select concat('a','b') from JMember  m", String.class).getResultList();
+            //하이버네이트는 해당 형식으로도 문자열을 더할 수 있다. uninject로 인텔리제이 옵션을 꺼야 사용 가능하다.
+            List<String> result2 = em.createQuery("select 'a' ||'b' from JMember  m", String.class).getResultList();
 
-            //nullIf 사용 -> 사용자 이름이 관리자면 Null반환  나머지는 본인의 이름 반환
-            List<String> result3 = em.createQuery("select nullif(m.username, '관리자') from JMember  m", String.class).getResultList();
+            //SUBSTRING - 문자열 자르기
+            List<String> result3 = em.createQuery("select substring(m.username, 2, 3) from JMember  m", String.class).getResultList();
+
+            //LOCATE - 문자열의 인덱스 반환
+            List<Integer> result4 = em.createQuery("select locate('cd','abcdefg') from JMember  m", Integer.class).getResultList();
+
+            //SIZE - 특정 컬렉션의 크기 반환
+            List<Integer> result5 = em.createQuery("select size(t.members) from JTeam  t", Integer.class).getResultList();
+
+            //INDEX -값타입 컬렉션에서 컬렉션의 위치값을 구할때 사용
+            @OrderColumn
+            List<Integer> result6 = em.createQuery("select index(t.members) from JTeam  t", Integer.class).getResultList();
 
             tx.commit();
         }catch (Exception e){
