9558fdceb8c7cb6947800721c12a69c540f99038
ENOCH YEOM <hykqo@naver.com>
2023-01-31 01:28:28 +0900

    JPQL 사용자 함수 정의 및 정의 함수 호출 예제

--- ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/JpaMain.java
@@ -29,25 +29,14 @@ public class JpaMain {
             em.flush();
             em.clear();
 
+            String query = "select function('group_concat', m.username) from JMember  m";
+            List<String> result = em.createQuery(query, String.class).getResultList();
+            for(String s : result) System.out.println("s = " + s);
 
-            //JPQL 기본함수
-            //CONCAT - 문자열 A와 B를 더한다
-            List<String> result1 = em.createQuery("select concat('a','b') from JMember  m", String.class).getResultList();
-            //하이버네이트는 해당 형식으로도 문자열을 더할 수 있다. uninject로 인텔리제이 옵션을 꺼야 사용 가능하다.
-            List<String> result2 = em.createQuery("select 'a' ||'b' from JMember  m", String.class).getResultList();
 
-            //SUBSTRING - 문자열 자르기
-            List<String> result3 = em.createQuery("select substring(m.username, 2, 3) from JMember  m", String.class).getResultList();
-
-            //LOCATE - 문자열의 인덱스 반환
-            List<Integer> result4 = em.createQuery("select locate('cd','abcdefg') from JMember  m", Integer.class).getResultList();
-
-            //SIZE - 특정 컬렉션의 크기 반환
-            List<Integer> result5 = em.createQuery("select size(t.members) from JTeam  t", Integer.class).getResultList();
-
-            //INDEX -값타입 컬렉션에서 컬렉션의 위치값을 구할때 사용
-            @OrderColumn
-            List<Integer> result6 = em.createQuery("select index(t.members) from JTeam  t", Integer.class).getResultList();
+            //하이버네이트에서는 해당 형식으로 지원을 한다 injectLanguage 를 끄고 해당 형식으로 작성해도 된다.
+            String query = "select group_concat(m.username) from JMember  m";
+            List<String> result = em.createQuery(query, String.class).getResultList();
 
             tx.commit();
         }catch (Exception e){
