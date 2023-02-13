37e8ab940e712e4becf945395c9fd9e8795aef50
ENOCH YEOM <hykqo@naver.com>
2023-02-13 20:32:37 +0900

    다형성쿼리 - type, treat사용법 예제

--- ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/com/jpabasic/ex1hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package com.jpabasic.ex1hellojpa;
 
+import com.jpabasic.ex1hellojpa.hellojpa.HelloItem;
+import com.jpabasic.ex1hellojpa.hellojpa.HelloMember;
 import com.jpabasic.ex1hellojpa.jpql.JMember;
 import com.jpabasic.ex1hellojpa.jpql.JTeam;
 
@@ -44,38 +46,15 @@ public class JpaMain {
             em.clear();
 
 
-
-//            //fetch join의 한계. 데이터 정합성이 맞지 않을 수 있다.
-//            List<JTeam> result1 = em.createQuery("select distinct t from JTeam t join fetch t.members", JTeam.class).getResultList();
-//            for(JTeam t : result1) System.out.println("t.getName() = " + t.getName() + ", [members="+t.getMembers()+"]");
-//
-//            //쓸거면 객체 그래프를 탐색하듯이만 쓰자.
-//            List<JTeam> result2 = em.createQuery("select distinct t from JTeam t join fetch t.members m join fetch m.team", JTeam.class).getResultList();
-//            for(JTeam t : result2) System.out.println("t.getName() = " + t.getName() + ", [members="+t.getMembers()+"]");
-
-            //컬렉션은 페치 조인 하면 페이징을 처리할 수 없다,
-//            List<JTeam> result3 = em.createQuery("select t from JTeam t join fetch t.members m", JTeam.class)
-//                    .setFirstResult(0)
-//                    .setMaxResults(1)
-//                    .getResultList();
-//            for(JTeam t : result3) System.out.println("t.getName() = " + t.getName() + ", [members="+t.getMembers()+"]");
-
-            //일대다인경우는 보통 다대일로 양방향을 매핑해놓은 경우가 많다.
-            //디대일로 조회를 하자.
-//            List<JMember> result4 = em.createQuery("select m from JMember m join fetch m.team t", JMember.class)
-//                    .setFirstResult(0)
-//                    .setMaxResults(1)
-//                    .getResultList();
-//            for(JMember m : result4) System.out.println("t.getName() = " + m.getTeam().getName() + ", [member="+m.getUsername()+"]");
-
-
-            //컬렉션 페이징을 죽어도 써야겠다. 그러면 이렇게 하자.
-            List<JTeam> result5 = em.createQuery("select t from JTeam t", JTeam.class)
-                    .setFirstResult(0)
-                    .setMaxResults(2)
+            //다형성 쿼리 type전략
+            List<HelloItem> result = em.createQuery("select i from HelloItem  i where type(i) in (HelloBook , HelloMovie)", HelloItem.class)
                     .getResultList();
-            for(JTeam t : result5) System.out.println("t.getName() = " + t.getName() + ", [members="+t.getMembers()+"]");
+            for(HelloItem i : result) System.out.println(i.getName());
 
+            //다형성 쿼리 treat전략 (다운캐스팅)
+            List<HelloItem> result2 = em.createQuery("select i from HelloItem  i where treat(i as HelloBook).Autoor = 'kim'", HelloItem.class)
+                    .getResultList();
+            for(HelloItem i : result) System.out.println(i.getName());
 
             tx.commit();
         }catch (Exception e){
